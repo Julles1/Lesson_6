a7ace278538627d13756f6ea3bbe90720da50785
Julles1 <yuliasubbotina93@mail.ru>
2022-07-01 22:40:11 +0300

    Задачи_1_и_2

---
+++ main.py
@@ -0,0 +1,35 @@
+#Задача_1
+number_int = int(input("Число: "))
+if number_int > 0:
+    print("Положительное")
+elif number_int < 0:
+    print("Отрицательное")
+else:
+    print("Ноль")
+
+#Задача_2
+number = int(input("Номер месяца- "))
+if number == 1:
+    print(31)
+elif number == 2:
+    print("Февраль")
+elif number == 3:
+    print(31)
+elif number == 4:
+    print(30)
+elif number == 5:
+    print(31)
+elif number == 6:
+    print(30)
+elif number == 7:
+    print(31)
+elif number == 8:
+    print(31)
+elif number == 9:
+    print(30)
+elif number == 10:
+    print(31)
+elif number == 11:
+    print(30)
+elif number == 12:
+    print(31)
