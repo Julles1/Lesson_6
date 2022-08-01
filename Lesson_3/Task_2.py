b2b71a4231411c488f53c058bba1d46a4dc54639
Julles1 <108360809+Julles1@users.noreply.github.com>
2022-08-01 20:45:34 +0300

    Delete Lesson_3 directory

--- Lesson_3/Task_2.py
+++
@@ -1,9 +0,0 @@
-from translate import Translator
-
-def translate_language_ru():
-    lang_ru = Translator(to_lang='ru')
-    user_input = input()
-    rus_translate = lang_ru.translate(user_input)
-    return rus_translate
-
-print(translate_language_ru())
